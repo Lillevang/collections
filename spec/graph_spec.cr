3458d01bc111f1023806c70efd44d50bc8e3e4ea
Jeppe Lillevang Salling <jeppe.lillevang@gmail.com>
2024-12-23 13:18:36 +0100

    Added graph functionality

--- spec/graph_spec.cr
+++ spec/graph_spec.cr
@@ -59,3 +59,103 @@ describe Collections::Graph do
     end
   end
 end
+describe "#fully_connected?" do
+  it "returns true for empty array" do
+    graph = Collections::Graph(Int32).new
+    graph.fully_connected?([] of Int32).should be_true
+  end
+
+  it "returns true for single node" do
+    graph = Collections::Graph(Int32).new
+    graph.add_node(1)
+    graph.fully_connected?([1]).should be_true
+  end
+
+  it "returns true for two connected nodes" do
+    graph = Collections::Graph(Int32).new
+    graph.add_edge(1, 2)
+    graph.fully_connected?([1, 2]).should be_true
+  end
+
+  it "returns true for three fully connected nodes" do
+    graph = Collections::Graph(Int32).new
+    graph.add_edge(1, 2)
+    graph.add_edge(2, 3)
+    graph.add_edge(1, 3)
+    graph.fully_connected?([1, 2, 3]).should be_true
+  end
+
+  it "returns false for three partially connected nodes" do
+    graph = Collections::Graph(Int32).new
+    graph.add_edge(1, 2)
+    graph.add_edge(2, 3)
+    # Missing edge between 1 and 3
+    graph.fully_connected?([1, 2, 3]).should be_false
+  end
+end
+
+describe "#find_subgraphs" do
+  it "finds all connected pairs" do
+    graph = Collections::Graph(Int32).new
+    graph.add_edge(1, 2)
+    graph.add_edge(2, 3)
+    graph.add_edge(1, 3)
+    subgraphs = graph.find_subgraphs(2)
+    subgraphs.size.should eq(3)
+    subgraphs.should contain([1, 2])
+    subgraphs.should contain([2, 3])
+    subgraphs.should contain([1, 3])
+  end
+
+  it "finds triangle subgraphs" do
+    graph = Collections::Graph(Int32).new
+    graph.add_edge(1, 2)
+    graph.add_edge(2, 3)
+    graph.add_edge(1, 3)
+    subgraphs = graph.find_subgraphs(3)
+    subgraphs.size.should eq(1)
+    subgraphs.first.should eq([1, 2, 3])
+  end
+
+  it "returns empty array for impossible size" do
+    graph = Collections::Graph(Int32).new
+    graph.add_edge(1, 2)
+    graph.find_subgraphs(3).should be_empty
+  end
+end
+
+describe "#largest_clique" do
+  it "returns empty array for empty graph" do
+    graph = Collections::Graph(Int32).new
+    graph.largest_clique.should be_empty
+  end
+
+  it "returns single node for isolated node" do
+    graph = Collections::Graph(Int32).new
+    graph.add_node(1)
+    graph.largest_clique.should eq([1])
+  end
+
+  it "finds clique in triangle" do
+    graph = Collections::Graph(Int32).new
+    graph.add_edge(1, 2)
+    graph.add_edge(2, 3)
+    graph.add_edge(1, 3)
+    graph.largest_clique.should eq([1, 2, 3])
+  end
+
+  it "finds largest clique in complex graph" do
+    graph = Collections::Graph(Int32).new
+    # Create a graph with a 4-clique and a triangle
+    graph.add_edge(1, 2)
+    graph.add_edge(1, 3)
+    graph.add_edge(1, 4)
+    graph.add_edge(2, 3)
+    graph.add_edge(2, 4)
+    graph.add_edge(3, 4)
+    graph.add_edge(5, 6)
+    graph.add_edge(6, 7)
+    graph.add_edge(5, 7)
+    graph.largest_clique.should eq([1, 2, 3, 4])
+  end
+end
--- src/graph/graph.cr
+++ src/graph/graph.cr
@@ -42,6 +42,62 @@ module Collections
       @adjacency_list.empty?
     end
 
+    def fully_connected?(nodes : Array(T)) : Bool
+      return true if nodes.empty?
+      nodes.each_with_index do |node1, i|
+        ((i + 1)...nodes.size).each do |j|
+          node2 = nodes[j]
+          neighbors = neighbors(node1).map(&.value)
+          return false unless neighbors.includes?(node2)
+        end
+      end
+      true
+    end
+
+    def find_subgraphs(size : Int32) : Array(Array(T))
+      nodes = @adjacency_list.keys.map(&.value)
+      subgraphs = [] of Array(T)
+
+      nodes.combinations(size).each do |combination|
+        subgraphs << combination if fully_connected?(combination)
+      end
+      subgraphs
+    end
+
+    def largest_clique : Array(T)
+      nodes = @adjacency_list.keys.map(&.value)                           # Get all node values
+      nodes = nodes.sort_by { |node| -(neighbors(node) || [] of T).size } # Sort by degree (descending)
+      max_clique = [] of T
+
+      backtrack_clique([] of T, nodes, max_clique) # Call the backtracking function
+      max_clique.sort
+    end
+
+    private def backtrack_clique(current_clique : Array(T), candidates : Array(T), max_clique : Array(T))
+      if candidates.empty?
+        if current_clique.size > max_clique.size
+          max_clique.clear
+          max_clique.concat(current_clique)
+        end
+        return
+      end
+
+      candidates.each_with_index do |candidate, i|
+        # Extend the current clique
+        new_clique = current_clique + [candidate]
+        # Filter candidates to keep only neighbors of the current candidate
+        node = find_node(candidate)
+        next unless node
+
+        new_candidates = candidates[(i + 1)..-1].select do |other|
+          neighbor_values = neighbors(candidate).map(&.value)
+          neighbor_values.includes?(other)
+        end
+        # Recurse
+        backtrack_clique(new_clique, new_candidates, max_clique)
+      end
+    end
+
     private def find_or_create_node(value : T) : Node(T)
       find_node(value) || add_node(value)
     end
